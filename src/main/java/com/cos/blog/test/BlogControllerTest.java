3cfe16155c6e4a97426cf2201c3bd24ee0f4a2db
git-hgjeong <code.hgjeong@gmail.com>
2022-02-25 12:30:18 +0900

    Step 13

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -1,6 +1,9 @@
 package com.cos.blog.test;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,25 @@ public class BlogControllerTest {
 	public String hello() {
 		return "<h1>hello spring boot</h1>";
 	}
+	
+	@GetMapping("/http/get")
+	public String getTest() {
+		return "get 요청";
+	}
+	
+	@PostMapping("/http/post")
+	public String postTest() {
+		return "post 요청";
+	}
+	
+	@PutMapping("/http/put")
+	public String putTest() {
+		return "put 요청";
+	}
+	
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete 요청";
+	}
+	
 }
