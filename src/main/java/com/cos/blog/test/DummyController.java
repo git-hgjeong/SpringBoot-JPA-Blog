c06ae1f290458e6e4a60981361324a921ff493a7
git-hgjeong <code.hgjeong@gmail.com>
2022-02-26 14:29:08 +0900

    Step 27. Get list & paging

--- src/main/java/com/cos/blog/test/DummyController.java
+++ src/main/java/com/cos/blog/test/DummyController.java
@@ -1,11 +1,17 @@
 package com.cos.blog.test;
 
-import java.util.function.Supplier;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -56,6 +62,30 @@ public class DummyController {
 		});				
 		
 		return user;
+	}
+	
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		List<User> list = userRepository.findAll();
+		return list;
+	}
+	
+	//http://localhost:8000/blog/dummy/user/list?page=0
+	@GetMapping("/dummy/user/list")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+		
+		Page<User> pagingUsers = userRepository.findAll(pageable);
+		List<User> list = pagingUsers.getContent();
+		
+		return list;
+	}	
+	
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		
+		System.out.println(requestUser.toString());
+		
+		return requestUser;
 	}
 	
 }
